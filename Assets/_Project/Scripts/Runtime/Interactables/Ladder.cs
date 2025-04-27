6af970edecd087d39f8f50c2af8bf5ada7901517
Yaygun <122031841+Yaygunn@users.noreply.github.com>
2025-04-27 04:40:52 +0300

    Climb logic

---
+++ Assets/_Project/Scripts/Runtime/Interactables/Ladder.cs
@@ -0,0 +1,54 @@
+using NJG.Runtime.Entity;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace NJG.Runtime.Interactables
+{
+    public class Ladder : MonoBehaviour
+    {
+        [FoldoutGroup("Pos"), SerializeField] 
+        private float _topHeight = 3;
+
+        [FoldoutGroup("Pos"), SerializeField] 
+        private Vector3 _bottomPos;
+        
+        [FoldoutGroup("Pos"), SerializeField] 
+        private Vector3 _topExitPos = new Vector3(0, 4, 1);
+        
+        public Vector3 GetClimbPosition(Vector3 playerPosition)
+        {
+            return transform.TransformPoint(_bottomPos);
+        }
+
+        public Quaternion GetClimbRotation()
+        {
+            return transform.rotation * Quaternion.Euler(0, 180, 0);
+        }
+
+        public Vector3 GetTopExitPos()
+        {
+            return transform.TransformPoint(_topExitPos);
+        }
+        public float GetBottomHeight()
+        {
+            return transform.TransformPoint(_bottomPos).y;
+        }
+
+        public float GetTopHeight()
+        {
+            return transform.TransformPoint(_bottomPos + new Vector3(0, _topHeight, 0)).y;
+        }
+        
+        #if UNITY_EDITOR
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawLine(transform.TransformPoint(_bottomPos),
+                transform.TransformPoint(_bottomPos + new Vector3(0, _topHeight, 0)));
+            Gizmos.DrawSphere(transform.TransformPoint(_bottomPos + new Vector3(0, _topHeight, 0)), 0.1f);
+            Gizmos.DrawSphere(transform.TransformPoint(_bottomPos), 0.1f);
+            Gizmos.DrawSphere(transform.TransformPoint(_topExitPos), 0.1f);
+        }
+        #endif
+    }
+}
