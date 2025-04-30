55f43da8d65233acf29cfe3befc461a6c25d3fa6
Vit <Ukr816@gmail.com>
2025-04-29 23:44:09 -0600

    Limited water source

--- Assets/_Project/Scripts/Runtime/Interactables/Water/WaterReservoir.cs
+++ Assets/_Project/Scripts/Runtime/Interactables/Water/WaterReservoir.cs
@@ -1,15 +1,43 @@
 using System;
+using KBCore.Refs;
 using NJG.Runtime.Entity;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace NJG.Runtime.Interactables
 {
+    [RequireComponent(typeof(Collider))]
     public class WaterReservoir : MonoBehaviour, IInteractable, IWaterSource
     {
+        [FoldoutGroup("References"), SerializeField, Anywhere]
+        private GameObject _waterVisual;
+        
+        [FoldoutGroup("Settings"), SerializeField]
+        private bool _unlimitedWater = true;
+        [FoldoutGroup("Settings"), SerializeField, HideIf(nameof(_unlimitedWater))]
+        private int _numberOfUses = 3;
+
+        private Collider _collider;
+        private int _remainingUses;
+        
+        public bool ContainsWater { get; private set; } = true;
+        
         public Transform Transform => transform;
         
         public event Action<string> OnTooltipTextChanged;
-        
+
+        private void Awake()
+        {
+            _collider = GetComponent<Collider>();
+            
+            _remainingUses = _numberOfUses;
+            if (!_unlimitedWater && _remainingUses <= 0)
+            {
+                _waterVisual.SetActive(false);
+                _collider.enabled = false;
+            }
+        }
+
         public void Interact(PlayerInventory playerInventory)
         {
             InteractionHelper.TryInteract(playerInventory, this);
@@ -18,7 +46,18 @@ namespace NJG.Runtime.Interactables
 
         public void FillWater(WaterContainer waterContainer)
         {
-            waterContainer.TryFillWater();
+            if (!ContainsWater)
+                return;
+
+            if (waterContainer.TryFillWater())
+                _remainingUses--;
+            
+            if (!_unlimitedWater && _remainingUses <= 0)
+            {
+                ContainsWater = false;
+                _waterVisual.SetActive(false);
+                _collider.enabled = false;
+            }
         }
 
         public string GetTooltipText(PlayerInventory playerInventory)
