44e9d414fa191ccb327259f7e2d5b93654166fb9
Yaygun <122031841+Yaygunn@users.noreply.github.com>
2025-05-03 16:03:10 +0300

    BoundPad & ILaunch interface

--- Assets/_Project/Scripts/Runtime/Interactables/Bounce/BouncePad.cs
+++ Assets/_Project/Scripts/Runtime/Interactables/Bounce/BouncePad.cs
@@ -10,14 +10,11 @@ namespace NJG.Runtime.Interactables
         [FoldoutGroup("Settings"), SerializeField]
         private Vector3 _force;
         
-        private const string PLAYER_TAG = "Player";
-        
         private void OnTriggerEnter(Collider other)
         {
-            // TODO: Possibly interface this, but should be ok for now
-            if (other.gameObject.TryGetComponent(out PlayerController player))
+            if (other.gameObject.TryGetComponent(out ILaunchable bouncable))
             {
-                player.AddForce(_force);
+                bouncable.Launch(_force);
             }
         }
     }
