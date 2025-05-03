ad862be2891c8c58a98b09e00c1e7a51094fa722
Yaygun <122031841+Yaygunn@users.noreply.github.com>
2025-05-03 22:59:07 +0300

    Battery pipe

---
+++ Assets/_Project/Scripts/Runtime/Interactables/Water/Pipe/PipeWaterReservoir.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using MEC;
+
+namespace NJG.Runtime.Interactables
+{
+    public class PipeWaterReservoir : WaterReservoir
+    {
+        [FoldoutGroup("Settings/Time"), SerializeField]
+        private float _fillTime = 0.5f;
+        [FoldoutGroup("Settings/Time"), SerializeField]
+        private float _emptyTime = 0.5f;
+
+        private CoroutineHandle _coroutineHandle;
+
+        private void Start()
+        {
+            ContainsWater = false;
+            _waterVisual.SetActive(false);
+        }
+
+        public void StartRecievingPipeWater()
+        {
+            Timing.KillCoroutines(_coroutineHandle);
+            _coroutineHandle = Timing.RunCoroutine(FillOperation());
+        }
+
+        public void StopRecievingPipeWater()
+        {
+            Timing.KillCoroutines(_coroutineHandle);
+            _coroutineHandle = Timing.RunCoroutine(EmptyOperation());
+        }
+
+        private IEnumerator<float> FillOperation()
+        {
+            yield return Timing.WaitForSeconds(_fillTime);
+            ContainsWater = true;
+            _waterVisual.SetActive(true);
+        }
+        
+        private IEnumerator<float> EmptyOperation()
+        {
+            yield return Timing.WaitForSeconds(_emptyTime);
+            ContainsWater = false;
+            _waterVisual.SetActive(false);
+        }
+    }
+}
