bd3d7f8c22f42cdbb627755cd1de98e50c8db98f
Vit <Ukr816@gmail.com>
2025-05-03 22:26:38 -0600

    KCC Setup

---
+++ Assets/_Project/Scripts/Runtime/Entities/Player/Player.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using KBCore.Refs;
+using Sirenix.OdinInspector;
+using Unity.Cinemachine;
+using UnityEngine;
+
+namespace NJG.Runtime.Entities
+{
+    public class Player : ValidatedMonoBehaviour
+    {
+        [FoldoutGroup("References"), SerializeField, Self]
+        private CharacterController _character;
+        [FoldoutGroup("References"), SerializeField, Anywhere]
+        private CharacterCamera _characterCamera;
+
+        private void Start()
+        {
+            //Cursor.lockState = CursorLockMode.Locked;
+            
+            _characterCamera.SetFollowTransform(_character.CameraFollowPoint);
+            
+            _characterCamera.IgnoredColliders.Clear();
+            _characterCamera.IgnoredColliders.AddRange(_character.GetComponentsInChildren<Collider>());
+        }
+    }
+}
