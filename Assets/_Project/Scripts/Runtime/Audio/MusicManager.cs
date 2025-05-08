223a96b815e3da821d61b82e078858b278f0e0e3
m3t4lp0w3r <antonygiuly2@hotmail.com>
2025-05-09 00:34:20 +0300

    Global parameter IsDay
    Changed the daynightcycle parameter with the IsDay parameter

--- Assets/_Project/Scripts/Runtime/Audio/MusicManager.cs
+++ Assets/_Project/Scripts/Runtime/Audio/MusicManager.cs
@@ -1,4 +1,5 @@
 using DistantLands.Cozy;
+using NJG.Runtime.Signals;
 using UnityEngine;
 using Zenject;
 
@@ -6,23 +7,18 @@ namespace NJG.Runtime.Audio
 {
     public class MusicManager : MonoBehaviour
     {
-        private const float _danNightCheckInterval = 5f;
-        private const string _dayNightCycleParam = "Day_Night_Cycle";
         private AudioManager _audioManager;
 
         [Inject]
-        private void Construct(AudioManager audioManager) => _audioManager = audioManager;
+        private void Construct(SignalBus signalBus, AudioManager audioManager)
+        {
+            _audioManager = audioManager;
+        }
 
         private void Start()
         {
             _audioManager.PlayPersistent(_audioManager.AudioData.Music);
-            InvokeRepeating(nameof(SyncCozyTime), 0f, _danNightCheckInterval);
-        }
-
-        private void SyncCozyTime()
-        {
-            float time = CozyWeather.instance.timeModule.currentTime;
-            _audioManager.SetGlobalParameter(_dayNightCycleParam, time);
+            DontDestroyOnLoad(gameObject);
         }
     }
 }
