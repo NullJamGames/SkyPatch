e00635d73da9e146b17543ba721af127581eac09
Vit <Ukr816@gmail.com>
2025-05-10 09:08:46 -0600

    Splash + Build

---
+++ Assets/_Project/Scripts/Runtime/Managers/Bootstrapper.cs
@@ -0,0 +1,21 @@
+﻿using NJG.Runtime.LevelChangeSystem;
+using UnityEngine;
+using Zenject;
+
+namespace NJG.Runtime.Managers
+{
+    public class Bootstrapper : MonoBehaviour
+    {
+        [SerializeField]
+        private string _nextSceneName = "1_Splash";
+
+        private LevelChangeManager _levelManager;
+
+        [Inject]
+        private void Construct(LevelChangeManager levelManager) => _levelManager = levelManager;
+
+        private void Start() => OnGameStarted();
+
+        private void OnGameStarted() => _levelManager.LoadScene(_nextSceneName);
+    }
+}
