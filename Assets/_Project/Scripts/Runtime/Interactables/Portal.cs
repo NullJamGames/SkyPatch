61260edc6f45d7d163c888fa8190654dcc1e9a79
Vit <Ukr816@gmail.com>
2025-05-10 04:05:55 -0600

    Redid level 4

---
+++ Assets/_Project/Scripts/Runtime/Interactables/Portal.cs
@@ -0,0 +1,14 @@
+﻿using NJG.Runtime.Interfaces;
+using UnityEngine;
+
+namespace NJG.Runtime.Interactables
+{
+    public class Portal : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.TryGetComponent(out IResetable resetable))
+                resetable.ResetState();
+        }
+    }
+}
